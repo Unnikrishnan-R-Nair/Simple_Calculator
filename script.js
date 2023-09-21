378e7f9253171580982bd7441e5e4682dd5ae666
Unnikrishnan R Nair <rajendranunnikrishnan7@gmail.com>
2023-09-21 19:07:38 +0530

    added DEL button

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 function buttonClick(val){
 
-    document.getElementById('screen').value+=val
-
+    document.getElementById('screen').value+=val;
+    
 }
+
 function clrButton(){
-    document.getElementById("screen").value=""
+    document.getElementById("screen").value="";
+    document.getElementById("screen1").value="";
+    
 }
+
+function backButton(){
+    let item = document.getElementById("screen").value;
+    newString = item.slice(0, item.length-1)
+    document.getElementById("screen").value = newString;
+    
+}
+
+let newResult = 0;
 function eqlClick(){
-    var text=document.getElementById("screen").value
-    var result=eval(text)
-    document.getElementById("screen").value=result
+    var text=document.getElementById("screen").value;
+    let result=eval(text);
+    document.getElementById("screen1").value=result;
+    document.getElementById("screen").value = ""
+    newResult = result; 
+    if (newResult > 0){
+        buttonClick(newResult);
+        document.getElementById("screen").value=newResult;
+    }
+    
 }
 
-
-
+if (newResult > 0){
+    let res = newResult + buttonClick(newResult);
+    document.getElementById("screen").value=newResult;
+}
 
 
 
